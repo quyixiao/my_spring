b14669cb741e95b51b3927f6f244d9cdc7ef9f3e
quyixiao <2621048238@qq.com>
2020-04-13 15:48:40 +0800

    提交修改

---
+++ src/main/java/org/springframework/cglib/core/internal/CustomizerRegistry.java
@@ -0,0 +1,50 @@
+package org.springframework.cglib.core.internal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.cglib.core.Customizer;
+import org.springframework.cglib.core.KeyFactoryCustomizer;
+
+public class CustomizerRegistry {
+    private final Class[] customizerTypes;
+    private Map<Class, List<KeyFactoryCustomizer>> customizers = new HashMap();
+
+    public CustomizerRegistry(Class[] customizerTypes) {
+        this.customizerTypes = customizerTypes;
+    }
+
+    public void add(KeyFactoryCustomizer customizer) {
+        Class<? extends KeyFactoryCustomizer> klass = customizer.getClass();
+        Class[] var3 = this.customizerTypes;
+        int var4 = var3.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Class type = var3[var5];
+            if (type.isAssignableFrom(klass)) {
+                List<KeyFactoryCustomizer> list = (List)this.customizers.get(type);
+                if (list == null) {
+                    this.customizers.put(type, list = new ArrayList());
+                }
+
+                ((List)list).add(customizer);
+            }
+        }
+
+    }
+
+    public <T> List<T> get(Class<T> klass) {
+        List<KeyFactoryCustomizer> list = (List)this.customizers.get(klass);
+        return list == null ? Collections.emptyList() : ( List )list;
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static CustomizerRegistry singleton(Customizer customizer) {
+        CustomizerRegistry registry = new CustomizerRegistry(new Class[]{Customizer.class});
+        registry.add((KeyFactoryCustomizer)customizer);
+        return registry;
+    }
+}
