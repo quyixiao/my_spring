8e4b0ed8ed7b38ab73673f3dc34f1cb6235bbf5c
quyixiao <2621048238@qq.com>
2020-05-24 21:58:25 +0800

    提交修改

--- src/main/java/org/springframework/remoting/support/RemoteInvocationBasedExporter.java
+++ src/main/java/org/springframework/remoting/support/RemoteInvocationBasedExporter.java
@@ -109,10 +109,24 @@ public abstract class RemoteInvocationBasedExporter extends RemoteExporter {
 	 * @param targetObject the target object to apply the invocation to
 	 * @return the invocation result
 	 * @see #invoke
+	 * 执行调用
+	 * 根据反序列化得到的RemoteInvocation对象中的信息，进行方法的调用，注意，在些用的实体并不是服务接口或者服务类，而是在之前初始化的时候
+	 * 构造的封装了我也很累接口以及服务类的代理
+	 * 完成了RemoteInvocation实例的提取，也就意味着可以通过RemoteInvocation实例中提供的信息进行方法的调用了
+	 * |
+	 * 对应方法的激活也就是invoke方法调用，虽然经过层层环绕，但是最终还是实现了一个我们熟知的调用invocation.invoke(targetObject),
+	 * 也就是执行RemoteInvocation类中的invoke方法，大致的逻辑还是通过RemoteInvocation中对应的方法信息在targetObject上去执行，
+	 * 此方法在分析RMI功能的时候己经分析过了，不赘述，但是对于当前方法的targetObject参数，此targetObject是代理类，调用代理类的时候
+	 * 需要考虑增强方法调用，这是读者需要注意的地方
+	 * 对于返回结果需要使用RemoteInvocationResult进行封装，之所以需要通过使用RemoteInvocationResult类型行统一封装
+	 * （3）半结果序列化对象写入输出流
+	 *
 	 */
 	protected RemoteInvocationResult invokeAndCreateResult(RemoteInvocation invocation, Object targetObject) {
 		try {
+			// 激活代理类中对应的invocation中的方法
 			Object value = invoke(invocation, targetObject);
+			// 封装的结果以便于序列化
 			return new RemoteInvocationResult(value);
 		}
 		catch (Throwable ex) {
